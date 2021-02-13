6d400c97497f7032b644b8dee6b1615dea62b713
Guylaxy <kritsana.mnp@gmail.com>
2021-02-14 01:28:30 +0700

    ก่อนเริ่มทำหน้าสมัคร Restaurant

--- src/RegisterPath/RegisterForRestaurant.js
+++ src/RegisterPath/RegisterForRestaurant.js
@@ -1,106 +1,61 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { StyleSheet, Text, View, ScrollView, TextInput, Modal, TouchableOpacity, Dimensions } from 'react-native';
 
 import { connect } from 'react-redux'
 
-import AnimatedMultistep from "react-native-animated-multistep";
+// import AnimatedMultistep from "react-native-animated-multistep";
+import { ProgressSteps, ProgressStep } from 'react-native-progress-steps';
 
-import Step1 from "./steps/step1";
-import Step2 from "./steps/step2";
-import Step3 from "./steps/step3";
-
-const allSteps = [
-    { name: "step 1", component: Step1 },
-    { name: "step 2", component: Step2 },
-    { name: "step 3", component: Step3 }
-];
 
 const RegisterForRestaurant = props => {
-    onNext = () => {
 
-        console.log("Next");
-    };
+    const [restaurant, setRestaurant] = useState({
+        username: '',
+        password: '',
+        firstname: '',
+        lastname: '',
+        gender: '',
+        age: '20',
+        phonenumber: '',
+        email: '',
+        facebook: '',
+        line: '',
 
-    /* define the method to be called when you go on back step */
-
-    onBack = () => {
-        console.log("Back");
-    };
-
-    /* define the method to be called when the wizard is finished */
-
-    finish = finalState => {
-        console.log(finalState);
-    };
-
+        isvalidUsername: false,
+        isvalidPassword: false,
+        isvalidFirstname: false,
+        isvalidPhonenumber: false,
+    })
 
     return (
 
         <View style={styles.container}>
-            <ScrollView horizontal={false} showsVerticalScrollIndicator={false} >
-
-                <View style={{ flex: 1 }}>
-                    <AnimatedMultistep
-                        steps={allSteps}
-                        onFinish={props.finish}
-                        onBack={props.onBack}
-                        onNext={props.onNext}
-                        comeInOnNext="fadeInRight"
-                        OutOnNext="fadeOutLeft"
-                        comeInOnBack="fadeInLeft"
-                        OutOnBack="fadeOutRight"
-                        duration={.025}
-                    />
-                </View>
-
-            </ScrollView>
+            <ProgressSteps>
+                <ProgressStep label="First Step">
+                    <View style={{ alignItems: 'center' }}>
+                        <Text>This is the content within step 1!</Text>
+                    </View>
+                </ProgressStep>
+                <ProgressStep label="Second Step">
+                    <View style={{ alignItems: 'center' }}>
+                        <Text>This is the content within step 2!</Text>
+                    </View>
+                </ProgressStep>
+                <ProgressStep label="Third Step">
+                    <View style={{ alignItems: 'center' }}>
+                        <Text>This is the content within step 3!</Text>
+                    </View>
+                </ProgressStep>
+            </ProgressSteps>
         </View>
     );
 }
 
 const styles = StyleSheet.create({
     container: { height: '100%', width: '100%', alignSelf: 'stretch', backgroundColor: '#FFF', alignItems: 'center', flex: 1, flexDirection: 'column' },
-    FormContainer: { width: '100%', marginVertical: 6 },
 
-    RegisterTitle: { flexWrap: 'wrap', justifyContent: 'center', flexDirection: 'row' },
-    TitleText: { fontFamily: 'pr-bold', fontSize: Dimensions.get('window').height < 1000 ? 18 : 20, marginLeft: 5, marginVertical: 20 },
-    FormFillTitle: { fontFamily: 'pr-reg', fontSize: Dimensions.get('window').height < 1000 ? 16 : 18 },
-    FillFormText: { fontFamily: 'pr-reg', color: '#838383', backgroundColor: '#FFFFE3', width: 300, paddingVertical: 5, paddingHorizontal: 20, height: Dimensions.get('window').height * .058, borderRadius: 15, fontSize: Dimensions.get('window').height < 1000 ? 14 : 16 },
-    FormContainerWrap: { alignSelf: 'center' },
-
-
-    consenseText: { fontFamily: 'pr-light', color: '#000', fontSize: 16, marginVertical: 5, },
-    ModalContainer: { backgroundColor: '#fff', margin: 50, padding: 40, borderRadius: 15 },
-    ModelBackground: { backgroundColor: '#000000aa', flex: 1, },
-    TouchBackButton: { backgroundColor: '#D4D4D4', borderRadius: 15, marginTop: 10, paddingVertical: '2%', width: '50%', alignSelf: 'center', },
-    closeButton: { fontFamily: 'pr-reg', textAlign: 'center' },
-
-    submitButton: { textAlign: 'center', fontFamily: 'pr-reg', backgroundColor: 'red', width: '30%', padding: '2%', borderRadius: 15, backgroundColor: '#FFFC1B', color: '#000', alignSelf: 'center' },
-    TouchReadButton: { marginBottom: '5%' },
-    readforSubmit: { fontFamily: 'pr-bold', textAlign: 'center', fontSize: 16 },
-    CheckBoxContainer: { flexDirection: 'row', justifyContent: 'center', marginBottom: '5%' }
 });
 
-const mapStatetoProps = (state) => {
-    return {
-        consense: state.consense
-    }
-}
 
-const mapDispatchtoProps = (dispatch) => {
-    return {
-        ShowConsense: () => {
-            dispatch({
-                type: "SHOW_CONSENSE",
-            })
-        },
-        CloseConsense: () => {
-            dispatch({
-                type: "CLOSE_CONSENSE",
-            })
-        },
 
-    }
-}
-
-export default connect(mapStatetoProps, mapDispatchtoProps)(RegisterForRestaurant);
+export default RegisterForRestaurant;
