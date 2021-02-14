e0a85161a225b0e9719c4ea16c191bfc1c84b1cd
Guylaxy <kritsana.mnp@gmail.com>
2021-02-14 15:28:43 +0700

    พัฒนาหน้าสมัครร้านอาหาร ก่อนทำการเชื่อม Hook

--- src/RegisterPath/RegisterForRestaurant.js
+++ src/RegisterPath/RegisterForRestaurant.js
@@ -27,6 +27,22 @@ const RegisterForRestaurant = props => {
         isvalidPhonenumber: false,
     })
 
+    const [restaurant, setRestaurant] = useState({
+        restaurantName: '',
+        address: '',
+        subdistrict: '',
+        amphoe: '',
+        province: '',
+        postalcode: '',
+        website: '',
+        isvalidRestaurantname: false,
+        isvalidAddress: false,
+        isvalidSubdistrict: false,
+        isvalidAmphoe: false,
+        isvalidProvince: false,
+        isvalidPostalcode: false
+    })
+
     return (
 
         <View style={styles.container}>
@@ -34,19 +50,22 @@ const RegisterForRestaurant = props => {
                 <ProgressStep label="บัญชี" nextBtnText='ถัดไป' nextBtnTextStyle={{ fontFamily: 'pr-reg', color: '#000' }}>
 
                     <View style={styles.FormContainer}>
-                        <View style={{ flexDirection: 'row' }}><Ionicons name="person-circle-outline" style={{ marginRight: 3 }} size={24} /><Text style={styles.FormFillTitle}>กำหนดชื่อผู้ใช้</Text></View>
+                        <View style={{ flexDirection: 'row', marginTop: 15 }}><Ionicons name="person-circle-outline" style={{ marginRight: 3 }} size={24} /><Text style={styles.FormFillTitle}>กำหนดชื่อผู้ใช้</Text></View>
                         <TextInput style={styles.FillFormText}></TextInput>
-                        <View style={{ flexDirection: 'row' }}><Ionicons name="lock-closed-outline" style={{ marginRight: 3 }} size={24} /><Text style={styles.FormFillTitle}>กำหนดรหัสผ่าน</Text></View>
+                        {user.isvalidUsername ? null : <View><Text style={styles.validText}>*ระบุ 4 - 16 ตัวอักษร</Text></View>}
+                        <View style={{ flexDirection: 'row', marginTop: 15 }}><Ionicons name="lock-closed-outline" style={{ marginRight: 3 }} size={24} /><Text style={styles.FormFillTitle}>กำหนดรหัสผ่าน</Text></View>
                         <TextInput style={styles.FillFormText} secureTextEntry={true} style={styles.FillFormText}></TextInput>
+                        {user.isvalidPassword ? null : <View><Text style={styles.validText}>*ระบุ 6 - 16 ตัวอักษร</Text></View>}
                     </View>
 
                 </ProgressStep>
                 <ProgressStep label="ข้อมูลผู้ใช้" nextBtnText='ถัดไป' previousBtnText="ย้อนกลับ" nextBtnTextStyle={{ fontFamily: 'pr-reg', color: '#000' }} previousBtnTextStyle={{ fontFamily: 'pr-reg', color: '#000' }}>
 
                     <View style={styles.FormContainer}>
-                        <View style={{ flexDirection: 'row' }}><Text style={styles.FormFillTitle}>ชื่อจริง</Text></View>
+                        <View style={{ flexDirection: 'row', marginTop: 15 }}><Text style={styles.FormFillTitle}>ชื่อจริง</Text></View>
                         <TextInput style={styles.FillFormText}></TextInput>
-                        <View style={{ flexDirection: 'row' }}><Text style={styles.FormFillTitle}>นามสกุล</Text></View>
+                        {user.isvalidUsername ? null : <View><Text style={styles.validText}>*ระบุไม่เกิน 16 ตัวอักษร</Text></View>}
+                        <View style={{ flexDirection: 'row', marginTop: 15 }}><Text style={styles.FormFillTitle}>นามสกุล</Text></View>
                         <TextInput style={styles.FillFormText} style={styles.FillFormText}></TextInput>
 
                         <View style={styles.FormContainerGenderTitle}><Text style={styles.FormFillTitleGen}>เพศ : </Text>{user.gender === '' ? <Text style={styles.genderTxt}>โปรดเลือก</Text> : (user.gender === 'male' ? <Text style={styles.genderTxt}>ชาย</Text> : <Text style={styles.genderTxt}>หญิง</Text>)}</View>
@@ -71,11 +90,12 @@ const RegisterForRestaurant = props => {
 
 
                     <View style={styles.FormContainer}>
-                        <View style={{ flexDirection: 'row' }}><Text style={styles.FormFillTitle}>เบอร์ติดต่อ</Text></View>
+                        <View style={{ flexDirection: 'row', marginTop: 15 }}><Text style={styles.FormFillTitle}>เบอร์ติดต่อ</Text></View>
                         <TextInput style={styles.FillFormText} keyboardType='numeric' secureTextEntry={true} style={styles.FillFormText}></TextInput>
-                        <View style={{ flexDirection: 'row' }}><Text style={styles.FormFillTitle}>อีเมล</Text></View>
+                        {user.isvalidPhonenumber ? null : <View><Text style={styles.validText}>*ระบุเบอร์ให้ถูกต้อง</Text></View>}
+                        <View style={{ flexDirection: 'row', marginTop: 15 }}><Text style={styles.FormFillTitle}>อีเมล</Text></View>
                         <TextInput style={styles.FillFormText} secureTextEntry={true} style={styles.FillFormText}></TextInput>
-                        <View style={{ flexDirection: 'row' }}><Text style={styles.FormFillTitle}>ไลน์</Text></View>
+                        <View style={{ flexDirection: 'row', marginTop: 15 }}><Text style={styles.FormFillTitle}>ไลน์</Text></View>
                         <TextInput style={styles.FillFormText} secureTextEntry={true} style={styles.FillFormText}></TextInput>
                     </View>
 
@@ -85,19 +105,27 @@ const RegisterForRestaurant = props => {
                         <View style={styles.FormContainer}>
                             <View style={{ flexDirection: 'row' }}><Text style={styles.FormFillTitle}>ชื่อร้านอาหาร</Text></View>
                             <TextInput style={styles.FillFormText}></TextInput>
+                            {restaurant.isvalidRestaurantname ? null : <View><Text style={styles.validText}>*ระบุ</Text></View>}
                         </View>
                         <View style={styles.FormContainer}>
-                            <View style={{ flexDirection: 'row' }}><Ionicons name='location-outline' size={24} style={{ marginRight: 5 }}></Ionicons><Text style={styles.FormFillTitle}>ที่อยู่ร้านอาหาร</Text></View>
+                            <View style={{ flexDirection: 'row', marginTop: 15 }}><Ionicons name='location-outline' size={24} style={{ marginRight: 5 }}></Ionicons><Text style={styles.FormFillTitle}>ที่อยู่ร้านอาหาร</Text></View>
                             <TextInput style={styles.FillFormText}></TextInput>
-                            <View style={{ flexDirection: 'row' }}><Text style={styles.FormFillTitle}>อำเภอ</Text></View>
+                            {restaurant.isvalidRestaurantname ? null : <View><Text style={styles.validText}>*ระบุ</Text></View>}
+                            <View style={{ flexDirection: 'row', marginTop: 15 }}><Text style={styles.FormFillTitle}>ตำบล</Text></View>
                             <TextInput style={styles.FillFormText}></TextInput>
-                            <View style={{ flexDirection: 'row' }}><Text style={styles.FormFillTitle}>จังหวัด</Text></View>
+                            {restaurant.isvalidSubdistrict ? null : <View><Text style={styles.validText}>*ระบุ</Text></View>}
+                            <View style={{ flexDirection: 'row', marginTop: 15 }}><Text style={styles.FormFillTitle}>อำเภอ</Text></View>
                             <TextInput style={styles.FillFormText}></TextInput>
-                            <View style={{ flexDirection: 'row' }}><Text style={styles.FormFillTitle}>รหัสไปรษณีย์</Text></View>
+                            {restaurant.isvalidAmphoe ? null : <View><Text style={styles.validText}>*ระบุ</Text></View>}
+                            <View style={{ flexDirection: 'row', marginTop: 15 }}><Text style={styles.FormFillTitle}>จังหวัด</Text></View>
                             <TextInput style={styles.FillFormText}></TextInput>
+                            {restaurant.isvalidProvince ? null : <View><Text style={styles.validText}>*ระบุ</Text></View>}
+                            <View style={{ flexDirection: 'row', marginTop: 15 }}><Text style={styles.FormFillTitle}>รหัสไปรษณีย์</Text></View>
+                            <TextInput style={styles.FillFormText}></TextInput>
+                            {restaurant.isvalidPostalcode ? null : <View><Text style={styles.validText}>*ระบุ</Text></View>}
                         </View>
                         <View style={styles.FormContainer}>
-                            <View style={{ flexDirection: 'row' }}><Text style={styles.FormFillTitle}>เว็บไซต์ของร้าน</Text></View>
+                            <View style={{ flexDirection: 'row', marginTop: 15 }}><Text style={styles.FormFillTitle}>เว็บไซต์ของร้าน</Text></View>
                             <TextInput style={styles.FillFormText}></TextInput>
                         </View>
                     </View>
@@ -110,9 +138,11 @@ const RegisterForRestaurant = props => {
 const styles = StyleSheet.create({
     container: { height: '100%', width: '100%', alignSelf: 'stretch', backgroundColor: '#FFF', alignItems: 'center', flex: 1, flexDirection: 'column' },
 
-    FillFormText: { fontFamily: 'pr-reg', color: '#838383', backgroundColor: '#FFFFE3', width: 220, paddingVertical: 5, paddingHorizontal: 20, borderRadius: 15, fontSize: Dimensions.get('window').height < 1000 ? 14 : 16, marginTop: 10, marginBottom: 12 },
+    FillFormText: { fontFamily: 'pr-reg', color: '#838383', backgroundColor: '#FFFFE3', width: 220, paddingVertical: 5, paddingHorizontal: 20, borderRadius: 15, fontSize: Dimensions.get('window').height < 1000 ? 14 : 16, marginTop: 10, marginBottom: 10 },
     FormContainer: { width: '100%', marginVertical: 20, marginLeft: 40 },
     FormFillTitle: { fontFamily: 'pr-reg', fontSize: Dimensions.get('window').height < 1000 ? 16 : 18, alignSelf: 'center' },
+    validText: { fontFamily: 'pr-reg', fontSize: Dimensions.get('window').height < 1000 ? 12 : 14, color: 'red', marginLeft: 10 },
+
 
     FormFillTitleGen: { fontFamily: 'pr-reg', fontSize: Dimensions.get('window').height < 1000 ? 16 : 18, marginRight: 5 },
     FormContainerGenderTitle: { width: '100%', marginVertical: 6, flexDirection: 'row' },
