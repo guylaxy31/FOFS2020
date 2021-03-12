82893bc085e91ad6926e890432d6073cf9d0937c
Guylaxy <kritsana.mnp@gmail.com>
2021-03-12 22:32:22 +0700

    Optimized UI App

--- src/Home/RecommendRestaurant.js
+++ src/Home/RecommendRestaurant.js
@@ -10,7 +10,7 @@ class RecommendRestaurant extends Component {
           <Image style={styles.imgBorder} source={{ uri: this.props.imageUri }}></Image>
         </View>
         <View>
-          <Text style={{ fontFamily: 'pr-light', marginTop: 10 }}>ร้าน{this.props.resName}</Text>
+          <Text style={{ fontFamily: 'pr-light', marginTop: 8 }}>ร้าน{this.props.resName}</Text>
         </View>
       </View>
     );
@@ -18,22 +18,8 @@ class RecommendRestaurant extends Component {
 }
 
 const styles = StyleSheet.create({
-  container: {
-    width: 150,
-    borderRadius: 15,
-    marginLeft: 20,
-    marginRight: 10,
-
-  }
-  ,
-  imgBorder: {
-
-
-    height: 150,
-    width: 150,
-    borderRadius: 15
-
-  },
+  container: { width: 144, borderRadius: 16, marginLeft: 16, marginRight: 8, },
+  imgBorder: { height: 144, width: 144, borderRadius: 16 },
 });
 
 export default RecommendRestaurant;
--- src/screens/DrawerContent.js
+++ src/screens/DrawerContent.js
@@ -25,9 +25,9 @@ export function DrawerContent(props) {
 
                     {AuthLogin == true ?
                         <View stlye={styles.userInfoSection}>
-                            <View style={{ flexDirection: 'row', marginTop: 15, paddingLeft: 15 }}>
-                                <MaterialCommunityIcons name="account" size={50} />
-                                <View style={{ marginLeft: 15, flexDirection: 'column' }}>
+                            <View style={{ flexDirection: 'row', marginTop: 16, paddingLeft: 16 }}>
+                                <MaterialCommunityIcons name="account" size={48} />
+                                <View style={{ marginLeft: 16, flexDirection: 'column' }}>
                                     <Title style={{ fontFamily: 'pr-light' }}><Text style={{ fontFamily: 'pr-light' }}>คุณ</Text>{database.firstname} {database.lastname}</Title>
                                     <Caption style={{ fontFamily: 'pr-light' }}>@{database.username}</Caption>
                                 </View>
@@ -35,35 +35,35 @@ export function DrawerContent(props) {
                         </View>
                         :
                         <View>
-                            <Text style={{ fontFamily: 'pr-light', textAlign: 'center', marginTop: 20, color: '#B1B067' }}>คุณยังไม่ได้เข้าสู่ระบบ</Text>
+                            <Text style={{ fontFamily: 'pr-light', textAlign: 'center', marginTop: 24, color: '#B1B067' }}>คุณยังไม่ได้เข้าสู่ระบบ</Text>
                         </View>}
 
                     <Drawer.Section style={styles.drawerSection}>
                         {/* ถ้าบทบาทเป็น'ร้านอาหาร'หน้าหลักจะเป็นหน้าเครื่องมือ 4 เมนูสำหรับร้าน */}
                         {/* นอกจากนั้นจะเป็น home หลักแสดงอาหารหมดทุกกรณี ทั้งล็อคอินและล็อคอินเป็นลูกค้า */}
-                        {database.role === 'restaurant' && AuthLogin === true ? <DrawerItem icon={({ color, size }) => (<MaterialCommunityIcons name="home-outline" color={color} size={size} />)} labelStyle={{ fontFamily: 'pr-reg' }} label="หน้าหลัก" onPress={() => props.navigation.navigate('RestaurantHome')} />
+                        {database.role === 'restaurant' && AuthLogin === true ? <DrawerItem icon={({ color, size }) => (<MaterialCommunityIcons name="home-outline" color={color} size={size} />)} labelStyle={styles.draweritemtext} label="หน้าหลัก" onPress={() => props.navigation.navigate('RestaurantHome')} />
                             :
-                            <DrawerItem icon={({ color, size }) => (<MaterialCommunityIcons name="home-outline" color={color} size={size} />)} labelStyle={{ fontFamily: 'pr-reg' }} label="หน้าหลัก" onPress={() => props.navigation.navigate('Homescreen')} />
+                            <DrawerItem icon={({ color, size }) => (<MaterialCommunityIcons name="home-outline" color={color} size={size} />)} labelStyle={styles.draweritemtext} label="หน้าหลัก" onPress={() => props.navigation.navigate('Homescreen')} />
                         }
                         {/* ถ้าเป็นร้านอาหาร ผ่านการล็อคอินแสดงหน้าตั้งค่า */}
-                        {database.role === 'restaurant' && AuthLogin === true ? <DrawerItem icon={({ color, size }) => (<MaterialCommunityIcons name="account-circle" color={color} size={size} />)} labelStyle={{ fontFamily: 'pr-reg' }} label="ตั้งค่าข้อมูลผู้ใช้" onPress={() => props.navigation.navigate('ProfileSetting')} />
+                        {database.role === 'restaurant' && AuthLogin === true ? <DrawerItem icon={({ color, size }) => (<MaterialCommunityIcons name="account-circle" color={color} size={size} />)} labelStyle={styles.draweritemtext} label="ตั้งค่าข้อมูลผู้ใช้" onPress={() => props.navigation.navigate('ProfileSetting')} />
                             :
                             null}
-                        {database.role === 'restaurant' && AuthLogin === true ? <DrawerItem icon={({ color, size }) => (<MaterialIcons name="store" size={size} color={color} />)} labelStyle={{ fontFamily: 'pr-reg' }} label="ตั้งค่าข้อมูลร้าน" onPress={() => props.navigation.navigate('RestaurantSetting')} />
+                        {database.role === 'restaurant' && AuthLogin === true ? <DrawerItem icon={({ color, size }) => (<MaterialIcons name="store" size={size} color={color} />)} labelStyle={styles.draweritemtext} label="ตั้งค่าข้อมูลร้าน" onPress={() => props.navigation.navigate('RestaurantSetting')} />
                             :
                             null}
 
                         {/* ตั้งค่าโปรไฟล์ถ้าหากเป็นลูกค้าให้เป็นการตั้งโปรไฟล์สำหรับลูกค้า ร้านอาหารให้เป็นการตั้งโปรไฟล์สำหรับร้านอาหาร */}
-                        {database.role === 'customer' && AuthLogin == true ? <DrawerItem icon={({ color, size }) => (<MaterialCommunityIcons name="account-circle" color={color} size={size} />)} labelStyle={{ fontFamily: 'pr-reg' }} label="ตั้งค่าโปรไฟล์" onPress={() => props.navigation.navigate('ProfileSetting')} /> : null}
+                        {database.role === 'customer' && AuthLogin == true ? <DrawerItem icon={({ color, size }) => (<MaterialCommunityIcons name="account-circle" color={color} size={size} />)} labelStyle={styles.draweritemtext} label="ตั้งค่าโปรไฟล์" onPress={() => props.navigation.navigate('ProfileSetting')} /> : null}
 
-                        {database.role != 'restaurant' || AuthLogin === false ? <DrawerItem icon={({ color, size }) => (<MaterialCommunityIcons name="help-circle" color={color} size={size} />)} labelStyle={{ fontFamily: 'pr-reg' }} label="วิธีการสั่งอาหาร" onPress={() => props.navigation.navigate('Tutorial')} /> : null}
+                        {database.role != 'restaurant' || AuthLogin === false ? <DrawerItem icon={({ color, size }) => (<MaterialCommunityIcons name="help-circle" color={color} size={size} />)} labelStyle={styles.draweritemtext} label="วิธีการสั่งอาหาร" onPress={() => props.navigation.navigate('Tutorial')} /> : null}
                         {/* navigate ไปยังหน้า customer report และ restaurant report จะต่างกัน */}
-                        {database.role === 'customer' && AuthLogin === true ? <DrawerItem icon={({ color, size }) => (<MaterialCommunityIcons name="alert" color={color} size={size} />)} labelStyle={{ fontFamily: 'pr-reg' }} label="แจ้งปัญหาที่พบ" onPress={() => props.navigation.navigate('CustomerReport')} />
+                        {database.role === 'customer' && AuthLogin === true ? <DrawerItem icon={({ color, size }) => (<MaterialCommunityIcons name="alert" color={color} size={size} />)} labelStyle={styles.draweritemtext} label="แจ้งปัญหาที่พบ" onPress={() => props.navigation.navigate('CustomerReport')} />
                             :
-                            database.role === 'restaurant' && AuthLogin === true ? <DrawerItem icon={({ color, size }) => (<MaterialCommunityIcons name="alert" color={color} size={size} />)} labelStyle={{ fontFamily: 'pr-reg' }} label="แจ้งปัญหาที่พบ" onPress={() => props.navigation.navigate('CustomerReport')} /> : null
+                            database.role === 'restaurant' && AuthLogin === true ? <DrawerItem icon={({ color, size }) => (<MaterialCommunityIcons name="alert" color={color} size={size} />)} labelStyle={styles.draweritemtext} label="แจ้งปัญหาที่พบ" onPress={() => props.navigation.navigate('CustomerReport')} /> : null
                         }
-                        <DrawerItem icon={({ color, size }) => (<MaterialCommunityIcons name="contacts" color={color} size={size} />)} labelStyle={{ fontFamily: 'pr-reg' }} label="ติดต่อ" onPress={() => props.navigation.navigate('Contact')} />
-                        <DrawerItem icon={({ color, size }) => (<MaterialIcons name="security" size={size} color={color} />)} labelStyle={{ fontFamily: 'pr-reg' }} label="สำหรับผู้พัฒนา" onPress={() => props.navigation.navigate('AdminHome')} />
+                        <DrawerItem icon={({ color, size }) => (<MaterialCommunityIcons name="contacts" color={color} size={size} />)} labelStyle={styles.draweritemtext} label="ติดต่อ" onPress={() => props.navigation.navigate('Contact')} />
+                        <DrawerItem icon={({ color, size }) => (<MaterialIcons name="security" size={size} color={color} />)} labelStyle={styles.draweritemtext} label="สำหรับผู้พัฒนา" onPress={() => props.navigation.navigate('AdminHome')} />
 
                     </Drawer.Section>
 
@@ -71,7 +71,7 @@ export function DrawerContent(props) {
                     <Drawer.Section>
                         <TouchableRipple onPress={() => toggleTheme()}>
                             <View style={styles.preference}>
-                                <Text style={{ fontFamily: 'pr-reg' }}>โหมดกลางคืน</Text>
+                                <Text style={styles.draweritemtext}>โหมดกลางคืน</Text>
 
                                 <View pointerEvents="none">
                                     <Switch value={isDarkTheme} />
@@ -86,8 +86,8 @@ export function DrawerContent(props) {
 
             <Drawer.Section style={styles.bottomDrawerSection}>
                 {AuthLogin == true ?
-                    <DrawerItem icon={({ color, size }) => (<MaterialCommunityIcons name="exit-to-app" color={color} size={size} />)} label="ออกจากระบบ" onPress={() => { setAuthLogin(false), setUser({ ...user, username: '', password: '' }) }} labelStyle={{ fontFamily: 'pr-reg' }} />
-                    : <DrawerItem icon={({ color, size }) => (<MaterialCommunityIcons name="login" color={color} size={size} />)} label="เข้าสู่ระบบ" onPress={() => props.navigation.navigate('LoginHome')} labelStyle={{ fontFamily: 'pr-reg' }} />}
+                    <DrawerItem icon={({ color, size }) => (<MaterialCommunityIcons name="exit-to-app" color={color} size={size} />)} label="ออกจากระบบ" labelStyle={styles.draweritemtext} onPress={() => { setAuthLogin(false), setUser({ ...user, username: '', password: '' }) }} labelStyle={{ fontFamily: 'pr-reg' }} />
+                    : <DrawerItem icon={({ color, size }) => (<MaterialCommunityIcons name="login" color={color} size={size} />)} label="เข้าสู่ระบบ" labelStyle={styles.draweritemtext} onPress={() => props.navigation.navigate('LoginHome')} labelStyle={{ fontFamily: 'pr-reg' }} />}
             </Drawer.Section >
         </View >
     )
@@ -95,14 +95,15 @@ export function DrawerContent(props) {
 
 const styles = StyleSheet.create({
     drawerContent: { flex: 1 },
-    userInfoSection: { paddingLeft: 20 },
+    draweritemtext: { fontFamily: 'pr-reg', fontSize: 16 },
+    userInfoSection: { paddingLeft: 16 },
     title: { fontSize: 16, marginTop: 3, fontFamily: 'pr-bold' },
     caption: { fontSize: 14, fontFamily: 'pr-light' },
-    row: { marginTop: 20, flexDirection: 'row', alignItems: 'center', paddingLeft: 15 },
-    section: { flexDirection: 'row', alignItems: 'center', marginRight: 15 },
+    row: { marginTop: 20, flexDirection: 'row', alignItems: 'center', paddingLeft: 16 },
+    section: { flexDirection: 'row', alignItems: 'center', marginRight: 16 },
 
-    drawerSection: { marginTop: 15 },
-    bottomDrawerSection: { marginBottom: 15, borderTopColor: '#FFF', borderTopWidth: 1 },
-    preference: { flexDirection: 'row', justifyContent: 'space-between', paddingVertical: 12, paddingHorizontal: 16 }
+    drawerSection: { marginTop: 16 },
+    bottomDrawerSection: { marginBottom: 16, borderTopColor: '#FFF', borderTopWidth: 1 },
+    preference: { flexDirection: 'row', justifyContent: 'space-between', paddingVertical: 8, paddingHorizontal: 16 }
 
 })
