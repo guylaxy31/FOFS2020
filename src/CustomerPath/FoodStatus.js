c4f6b870a7ebae2b8e39b42af4008573709eb3d1
Guylaxy <kritsana.mnp@gmail.com>
2021-03-13 16:13:06 +0700

    อัปเดต UI ตรวจสถานะออเดอร์

--- src/CustomerPath/FoodStatus.js
+++ src/CustomerPath/FoodStatus.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { StyleSheet, Text, View, TouchableOpacity, Dimensions, ScrollView } from 'react-native';
-
+import { MaterialCommunityIcons } from '@expo/vector-icons';
 
 const FoodStatus = props => {
 
@@ -12,8 +12,9 @@ const FoodStatus = props => {
 
                 <View style={styles.CardContainer}>
 
-                    <View style={{ marginBottom: 30 }}>
-                        <Text style={styles.StatusHeaderText}>ตรวจสถานะออเดอร์</Text>
+                    <View style={{ marginBottom: 32, flexDirection: 'row', alignItems: 'center', width: '100%', justifyContent: 'center', paddingVertical: 16, backgroundColor: '#E6E5CE', borderRadius: 16 }}>
+                        <MaterialCommunityIcons name="silverware" size={32} color="black" style={{ marginRight: 8 }} />
+                        <Text style={styles.StatusHeaderText}>สถานะออเดอร์</Text>
                     </View>
                     <View style={styles.StatusHeaderContainer}>
                         <Text style={styles.HeaderTextLeft}>#</Text>
@@ -25,13 +26,19 @@ const FoodStatus = props => {
                         <Text style={styles.ValueTextLeft}>152</Text>
                         <Text style={styles.ValueTextCenter}>11:02</Text>
                         <Text style={styles.ValueTextCenter}>กับข้าวปาณี</Text>
+                        <Text style={styles.ValueTextRight}>รอรับออเดอร์</Text>
+                    </View>
+                    <View style={styles.StatusValueContainer}>
+                        <Text style={styles.ValueTextLeft}>153</Text>
+                        <Text style={styles.ValueTextCenter}>12:06</Text>
+                        <Text style={styles.ValueTextCenter}>ไก่ทอดหาดเล็ด</Text>
                         <Text style={styles.ValueTextRight}>กำลังปรุงอาหาร</Text>
                     </View>
                     <View style={styles.StatusValueContainer}>
                         <Text style={styles.ValueTextLeft}>153</Text>
                         <Text style={styles.ValueTextCenter}>12:06</Text>
                         <Text style={styles.ValueTextCenter}>ไก่ทอดหาดเล็ด</Text>
-                        <Text style={styles.ValueTextRight}>รับออเดอร์</Text>
+                        <Text style={styles.ValueTextRight}>อาหารเสร็จแล้ว</Text>
                     </View>
 
                 </View>
@@ -46,19 +53,19 @@ const FoodStatus = props => {
 
 const styles = StyleSheet.create({
     container: { height: '100%', width: '100%', alignSelf: 'stretch', backgroundColor: '#FFF', alignItems: 'center', justifyContent: 'center' },
-    CardContainer: { marginTop: 50, flexDirection: 'column', alignItems: 'center', alignSelf: 'center', margin: 20, width: 500 / Dimensions.get('window').width + 380, backgroundColor: "#FFF", shadowColor: 'black', shadowOffset: { width: 0, height: 2 }, shadowRadius: 6, elevation: 3, shadowOpacity: 0.26, paddingVertical: 50, borderRadius: 15 },
+    CardContainer: { marginTop: 32, flexDirection: 'column', alignItems: 'center', alignSelf: 'center', margin: 24, width: 376, backgroundColor: "#FFF", shadowColor: 'black', shadowOffset: { width: 0, height: 2 }, shadowRadius: 6, elevation: 3, shadowOpacity: 0.26, borderRadius: 16, paddingBottom: 24 },
 
-    StatusHeaderText: { fontFamily: 'pr-bold', fontSize: Dimensions.get('window').height < 1000 ? 20 : 22 },
+    StatusHeaderText: { fontFamily: 'pr-bold', fontSize: 18 },
 
-    StatusHeaderContainer: { width: '100%', flexDirection: 'row', justifyContent: 'space-between', marginBottom: 20 },
-    HeaderTextLeft: { fontFamily: 'pr-reg', fontSize: Dimensions.get('window').height < 1000 ? 16 : 18, marginLeft: 40 },
-    HeaderTextCenter: { fontFamily: 'pr-reg', fontSize: Dimensions.get('window').height < 1000 ? 16 : 18 },
-    HeaderTextRight: { fontFamily: 'pr-reg', fontSize: Dimensions.get('window').height < 1000 ? 16 : 18, marginRight: 50 },
+    StatusHeaderContainer: { width: '100%', flexDirection: 'row', justifyContent: 'space-between', marginBottom: 24 },
+    HeaderTextLeft: { fontFamily: 'pr-reg', fontSize: 16, marginLeft: 40 },
+    HeaderTextCenter: { fontFamily: 'pr-reg', fontSize: 16 },
+    HeaderTextRight: { fontFamily: 'pr-reg', fontSize: 16, marginRight: 48 },
 
-    StatusValueContainer: { width: '100%', flexDirection: 'row', justifyContent: 'space-between', marginVertical: 10 },
-    ValueTextLeft: { fontFamily: 'pr-reg', fontSize: Dimensions.get('window').height < 1000 ? 16 : 18, marginLeft: 40, color: '#888888' },
-    ValueTextCenter: { fontFamily: 'pr-reg', fontSize: Dimensions.get('window').height < 1000 ? 16 : 18, color: '#888888', alignItems: 'center' },
-    ValueTextRight: { fontFamily: 'pr-reg', fontSize: Dimensions.get('window').height < 1000 ? 16 : 18, marginRight: 30, color: '#000' }
+    StatusValueContainer: { width: '100%', flexDirection: 'row', justifyContent: 'space-between', marginVertical: 8 },
+    ValueTextLeft: { fontFamily: 'pr-reg', fontSize: 16, marginLeft: 40, color: '#888888' },
+    ValueTextCenter: { fontFamily: 'pr-reg', fontSize: 16, color: '#888888', alignItems: 'center', },
+    ValueTextRight: { fontFamily: 'pr-reg', fontSize: 16, marginRight: 32, color: '#000' }
 
 });
 
