407b98f42d00865a58637f09f10992cdc813eeca
Guylaxy <kritsana.mnp@gmail.com>
2021-03-19 16:14:40 +0700

    กราฟ 24 ชม.

--- src/RestaurantPath/AnalyticMain.js
+++ src/RestaurantPath/AnalyticMain.js
@@ -1,5 +1,7 @@
 import React, { Component, useState, useEffect } from 'react';
-import { StyleSheet, Text, View, TouchableOpacity, Dimensions, ScrollView } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity, Dimensions, ScrollView, useWindowDimensions } from 'react-native';
+import { TabView, SceneMap } from 'react-native-tab-view';
+
 import {
     LineChart,
     BarChart,
@@ -11,7 +13,289 @@ import {
 
 const windowWidth = Dimensions.get('window').width;
 
-const AnalyticMain = (props) => {
+export default function AnalyticMain() {
+
+    const layout = useWindowDimensions();
+
+    const [index, setIndex] = React.useState(0);
+    const [routes] = React.useState([
+        { key: 'first', title: '24 ชม.' },
+        { key: 'second', title: 'วันในสัปดาห์' },
+        { key: 'third', title: 'เดือน' },
+    ]);
+
+    const renderScene = SceneMap({
+        first: DayRoute,
+        second: WeekRoute,
+        third: MonthRoute,
+    });
+
+    return (
+        <TabView
+            swipeEnabled={false}
+            navigationState={{ index, routes }}
+            renderScene={renderScene}
+            onIndexChange={setIndex}
+            initialLayout={{ width: layout.width }}
+
+        />
+
+    );
+}
+
+const DayRoute = (props) => {
+
+    const labellist = ["05:00", "06:00", "07:00", "08:00", "09:00", "10:00", "11:00", "12:00", "13:00", "14:00", "15:00", "16:00", "17:00", "18:00", "19:00", "20:00", "21:00", "22:00", "23:00", "00:00", "01:00", "02:00", "03:00", "04:00"]
+    const datalist = [0, 0, 0, 0, 50, 120, 750, 955, 815, 620, 375, 350, 250, 120, 80, 40, 0, 0, 0, 0, 0, 0, 0, 0,]
+
+    const [maxtotal, setmaxtotal] = useState()
+    const [maxtimeindex, setmaxtimeindex] = useState()
+    const [prevmaxtotal, setprevmaxtotal] = useState(485)
+    const [daytotal, setdaytotal] = useState()
+
+    function findMaxTotal(arrlist) {
+        setmaxtotal(Math.max.apply(null, arrlist))
+        setdaytotal(datalist.reduce(function (acc, val) { return acc + val; }, 0))
+    }
+
+    // function findMaxIndex(maxval) {
+    //     setmaxattime(datalist.findIndex(maxval))
+    // }
+
+    useEffect(() => {
+        findMaxTotal(datalist)
+        let tempindex = datalist.indexOf(maxtotal)
+        setmaxtimeindex(tempindex)
+    }, [maxtotal])
+
+
+    return (
+        <ScrollView showsVerticalScrollIndicator={false} showsHorizontalScrollIndicator={false} style={{ backgroundColor: '#FFF' }}>
+            <View style={styles.container}>
+                <View style={styles.CardContainer}>
+                    <View><Text style={styles.TotalText}>ยอดรวมของร้านอาหาร</Text></View>
+                    <View><Text style={styles.TextTitleHeader}>ภาพรวม (24 ชม.)</Text></View>
+                    <View style={styles.LineChartContainer}>
+                        <LineChart
+                            data={{
+                                datasets: [
+                                    {
+                                        data: datalist
+                                    }
+                                ]
+                            }}
+                            width={windowWidth}// from react-native
+                            height={280}
+
+                            yAxisSuffix=" ฿"
+                            yAxisInterval={1} // optional, defaults to 1
+                            chartConfig={{
+                                backgroundColor: "#e26a00",
+                                backgroundGradientFrom: "#FFFFEF",
+                                backgroundGradientTo: "#F1F1E7",
+                                decimalPlaces: 2, // optional, defaults to 2dp
+                                color: (opacity = 1) => `rgba(87, 195, 192, ${opacity})`,
+                                labelColor: (opacity = 1) => `rgba(0, 0, 0, ${opacity})`,
+                                style: {
+
+                                    borderRadius: 16,
+
+                                },
+                                propsForDots: {
+                                    r: "2",
+                                    strokeWidth: "2",
+                                    stroke: "#378885"
+                                }
+                            }}
+                            bezier
+                            style={{
+                                marginVertical: 8,
+                                borderRadius: 16
+                            }}
+
+                        />
+                    </View>
+
+                    <View style={styles.dataContainer}>
+
+                        <View style={styles.column1}>
+                            <Text style={styles.changeFont}>ยอดสูงสุดที่ช่วง</Text>
+                            <Text style={styles.changeFont}>เป็นจำนวน</Text>
+                            <Text style={[styles.changeFont, { fontFamily: 'pr-bold', marginTop: 8 }]}>ยอดรวมตอนนี้</Text>
+                        </View>
+                        <View style={styles.column2}>
+                            <Text style={styles.changeFont}>{maxtimeindex == undefined ? <Text>กำลังโหลด...</Text> : labellist[maxtimeindex]}</Text>
+                            <Text style={styles.changeFont}>{maxtotal == undefined ? <Text>กำลังโหลด...</Text> : maxtotal}</Text>
+                            <Text style={[styles.changeFont, { marginTop: 8, fontFamily: 'pr-bold' }]}>{daytotal == undefined ? <Text>กำลังคำนวณ...</Text> : daytotal} </Text>
+                        </View>
+                        <View>
+                            <Text style={styles.changeFont}>น.</Text>
+                            <Text style={styles.changeFont}>บาท</Text>
+                            <Text style={[styles.changeFont, { marginTop: 8, fontFamily: 'pr-bold' }]}>บาท</Text>
+                        </View>
+                    </View>
+
+                </View>
+            </View>
+
+            <View style={{ marginBottom: 24 }}><Text style={[styles.TextTitleHeader, { textAlign: 'center' }]}>กราฟละเอียด</Text></View >
+            <View style={{ marginLeft: 20 }}><Text style={styles.TextSubTitleHeader}>Tip : กราฟสามารถเลื่อนซ้ายขวา</Text></View>
+
+
+            <ScrollView style={{ backgroundColor: '#FFF' }} horizontal={true} showsHorizontalScrollIndicator={false}>
+                <View style={styles.LineChartContainer2}>
+
+                    <LineChart
+                        data={{
+                            labels: labellist,
+                            datasets: [
+                                {
+                                    data: datalist
+                                }
+                            ]
+                        }}
+                        width={1000}// from react-native
+                        height={280}
+
+                        yAxisSuffix=" ฿"
+                        yAxisInterval={1} // optional, defaults to 1
+                        chartConfig={{
+                            backgroundColor: "#e26a00",
+                            backgroundGradientFrom: "#FFFFEF",
+                            backgroundGradientTo: "#F1F1E7",
+                            decimalPlaces: 2, // optional, defaults to 2dp
+                            color: (opacity = 1) => `rgba(87, 195, 192, ${opacity})`,
+                            labelColor: (opacity = 1) => `rgba(0, 0, 0, ${opacity})`,
+                            style: {
+
+                                borderRadius: 16,
+
+                            },
+                            propsForDots: {
+                                r: "6",
+                                strokeWidth: "2",
+                                stroke: "#378885"
+                            }
+                        }}
+                        bezier
+                        style={{
+                            marginVertical: 8,
+                            borderRadius: 16
+                        }}
+
+                    />
+
+                </View>
+
+            </ScrollView>
+        </ScrollView >
+    );
+
+}
+
+const WeekRoute = (props) => {
+
+    const labellist = ["จันทร์", "อังคาร", "พุธ", "พฤหัส", "ศุกร์", "เสาร์", "อาทิตย์"]
+    const datalist = [1300, 1225, 11000, 1750, 1650, 860, 750]
+
+    const [maxtotal, setmaxtotal] = useState()
+    const [maxtimeindex, setmaxtimeindex] = useState()
+    const [prevmaxtotal, setprevmaxtotal] = useState(485)
+
+
+    function findMaxTotal(arrlist) {
+        setmaxtotal(Math.max.apply(null, arrlist))
+    }
+
+    // function findMaxIndex(maxval) {
+    //     setmaxattime(datalist.findIndex(maxval))
+    // }
+
+    useEffect(() => {
+        findMaxTotal(datalist)
+        let tempindex = datalist.indexOf(maxtotal)
+        setmaxtimeindex(tempindex)
+    }, [maxtotal])
+
+
+    return (
+        <ScrollView showsVerticalScrollIndicator={false} showsHorizontalScrollIndicator={false} style={{ backgroundColor: '#FFF' }}>
+            <View style={styles.container}>
+                <View style={styles.CardContainer}>
+                    <View><Text style={styles.TotalText}>ยอดรวมของร้านอาหาร</Text></View>
+                    <View><Text style={styles.TextTitleHeader}>(ในสัปดาห์นี้)</Text></View>
+                    <View style={styles.LineChartContainer}>
+                        <LineChart
+                            data={{
+                                labels: labellist,
+                                datasets: [
+                                    {
+                                        data: datalist
+                                    }
+                                ]
+                            }}
+                            width={windowWidth}// from react-native
+                            height={280}
+
+                            yAxisSuffix=" ฿"
+                            yAxisInterval={1} // optional, defaults to 1
+                            chartConfig={{
+                                backgroundColor: "#e26a00",
+                                backgroundGradientFrom: "#FFFFEF",
+                                backgroundGradientTo: "#F1F1E7",
+                                decimalPlaces: 2, // optional, defaults to 2dp
+                                color: (opacity = 1) => `rgba(87, 195, 192, ${opacity})`,
+                                labelColor: (opacity = 1) => `rgba(0, 0, 0, ${opacity})`,
+                                style: {
+
+                                    borderRadius: 16,
+
+                                },
+                                propsForDots: {
+                                    r: "2",
+                                    strokeWidth: "2",
+                                    stroke: "#378885"
+                                }
+                            }}
+                            bezier
+                            style={{
+                                marginVertical: 8,
+                                borderRadius: 16
+                            }}
+
+                        />
+                    </View>
+
+                    <View style={styles.dataContainer}>
+
+                        <View style={styles.column1}>
+                            <Text style={styles.changeFont}>ยอดสูงสุดคือวัน</Text>
+                            <Text style={styles.changeFont}>ยอดทั้งหมด</Text>
+                            <Text style={styles.changeFont}>เทียบกับวันก่อนหน้า</Text>
+                        </View>
+                        <View style={styles.column2}>
+                            <Text style={styles.changeFont}>{maxtimeindex == undefined ? <Text>กำลังโหลด...</Text> : labellist[maxtimeindex]}</Text>
+                            <Text style={styles.changeFont}>{maxtotal == undefined ? <Text>กำลังโหลด...</Text> : maxtotal}</Text>
+                            <Text style={styles.changeFont}>{maxtotal == undefined || maxtimeindex == undefined ? <Text>กำลังคำนวณ...</Text> : (datalist[maxtimeindex - 1] > maxtotal ? <Text style={{ color: 'red' }}>{maxtotal - (datalist[maxtimeindex - 1])}</Text> : <Text style={{ color: 'green' }}>+{maxtotal - (datalist[maxtimeindex - 1])}</Text>)}</Text>
+                        </View>
+                        <View>
+                            <Text style={styles.changeFont}></Text>
+                            <Text style={styles.changeFont}>บาท</Text>
+                            <Text style={styles.changeFont}>บาท</Text>
+                        </View>
+                    </View>
+
+                </View>
+            </View>
+
+
+        </ScrollView >
+    );
+
+}
+
+const MonthRoute = (props) => {
+
     const labellist = ["05:00", "06:00", "07:00", "08:00", "09:00", "10:00", "11:00", "12:00", "13:00", "14:00", "15:00", "16:00", "17:00", "18:00", "19:00", "20:00", "21:00", "22:00", "23:00", "00:00", "01:00", "02:00", "03:00", "04:00"]
     const datalist = [0, 0, 0, 0, 50, 120, 750, 955, 815, 620, 375, 350, 250, 120, 80, 40, 0, 0, 0, 0, 0, 0, 0, 0,]
 
@@ -33,7 +317,6 @@ const AnalyticMain = (props) => {
         let tempindex = datalist.indexOf(maxtotal)
         setmaxtimeindex(tempindex)
     }, [maxtotal])
-
 
 
     return (
@@ -88,7 +371,7 @@ const AnalyticMain = (props) => {
                         <View style={styles.column1}>
                             <Text style={styles.changeFont}>ยอดสูงสุดที่ช่วง</Text>
                             <Text style={styles.changeFont}>ยอดทั้งหมด</Text>
-                            <Text style={styles.changeFont}>เทียบกับก่อนหน้า</Text>
+                            <Text style={styles.changeFont}>เทียบกับวันก่อนหน้า</Text>
                         </View>
                         <View style={styles.column2}>
                             <Text style={styles.changeFont}>{maxtimeindex == undefined ? <Text>กำลังโหลด...</Text> : labellist[maxtimeindex]}</Text>
@@ -160,6 +443,8 @@ const AnalyticMain = (props) => {
 
 }
 
+
+
 const styles = StyleSheet.create({
     container: { flex: 1, height: '100%', width: '100%', alignSelf: 'stretch', backgroundColor: '#FFF', alignItems: 'center', justifyContent: 'center', },
     TextTitleHeader: { fontFamily: 'pr-reg', fontSize: 18 },
@@ -174,5 +459,4 @@ const styles = StyleSheet.create({
     LineChartContainer2: { backgroundColor: '#FFF', flexDirection: 'row', marginBottom: '5%', marginLeft: 20, marginRight: 20 }
 });
 
-export default AnalyticMain
 
