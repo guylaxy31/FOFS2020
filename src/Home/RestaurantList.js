1d9a04a933102197b2731648c91b2686f7bb50b7
Attachai Pusin <attachaipusin@Attachais-MacBook-Air.local>
2021-03-22 13:16:18 +0700

    ีupdate restaurant list

--- src/Home/RestaurantList.js
+++ src/Home/RestaurantList.js
@@ -1,13 +1,46 @@
-import React from 'react';
-import { StyleSheet, Text, View, TouchableOpacity, Dimensions, ScrollView, Image, FlatList } from 'react-native';
-
-
+import React, { useEffect, useState, useContext, useCallback } from 'react';
+import { StyleSheet, Text, View, TouchableOpacity, Dimensions, ScrollView, Image, FlatList, RefreshControl } from 'react-native';
+import baseUrl from '../../assets/common/baseUrl';
+import axios from "axios";
+import AuthGlobal from '../Context/Store/AuthGlobal'
+import { useFocusEffect } from '@react-navigation/native'
+const wait = (timeout) => {
+    return new Promise(resolve => setTimeout(resolve, timeout));
+}
 const RestaurantList = (props) => {
-
+    console.log(props);
+    const [restaurants, setRestaurants] = useState([]);
+    const [refreshing, setRefreshing] = useState(false);
+    const [loading, setLoading] = useState(true);
+    const context = useContext(AuthGlobal);
+    const onRefresh = useCallback(() => {
+        setRefreshing(true);
+        wait(2000).then(() => setRefreshing(false));
+    }, []);
+    useFocusEffect((
+        useCallback(
+            () => {
+                axios
+                    .get(`${baseUrl}home`)
+                    .then((res) => {
+                        setRestaurants(res.data);
+                        setLoading(false);
+                    })
+            },
+            [],
+        )
+    ))
+    console.log(restaurants);
     return (
 
         <View style={styles.container}>
-            <ScrollView style={{ width: '100%' }}>
+            <ScrollView style={{ width: '100%' }}
+            refreshControl={
+                <RefreshControl
+                  refreshing={refreshing}
+                  onRefresh={onRefresh}
+                />
+              }>
 
                 <FlatList
                     data={restaurants}
@@ -15,8 +48,10 @@ const RestaurantList = (props) => {
                     renderItem={({ item }) =>
                         <TouchableOpacity style={styles.touchContainer}>
                             <View style={styles.restaurantContainer}>
-                                <View style={styles.containImage}></View>
-                                <View style={styles.containText}><Text style={styles.restText}>ร้านกินชากาแฟ</Text></View>
+                                <View style={styles.containImage}>
+                                <Image style={styles.imgBorder} source={{ uri: item.res_image }}></Image>
+                                </View>
+                                <View style={styles.containText}><Text style={styles.restText}>{item.restaurant_name}</Text></View>
                             </View>
                         </TouchableOpacity>
                     }
